ffd18c42b19e4a8f3517a9d770711cdbb79b4f54
Eric Granillo <ericgranillo@users.noreply.github.com>
2022-10-14 17:39:57 +0000

    challenge number 01

---
+++ lab_challenge/challenge01.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - printing challenge"""
+
+
+def main():
+
+    # collect string name input from the user
+
+    user_input_name = input("Hello user! What is your name?")
+    
+
+    # collect string day input from the user
+
+    user_input_day = input("Do you know what day of the week it is?")
+
+
+    # print() greeting with user's information
+
+    print("Hello ",user_input_name,"!"," Hope you're having a great ",user_input_day,"!",sep="")
+
+
+main()
+ 
+
+
