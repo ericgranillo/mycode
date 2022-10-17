f21ed139230ea6dff1fb28e28dee9c7b5a31135c
Eric Granillo <ericgranillo@users.noreply.github.com>
2022-10-17 21:26:30 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/ python3
+
+# list containing tree items
+iplist = [  5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# returns the ip address
+#print("The first item in the list (IP): " + my_list[0] )
+
+# returns the port number
+#print("The second item in the list (port): " + str(my_list[1]) )
+
+# returns the last item in the list
+#print("The last item in the list (state): " + my_list[2] )
+
+print("IP addresses: " + iplist[3] + ", and " + iplist[4])
+
