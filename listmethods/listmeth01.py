6e8d55da1ffa815f9ef40a2a2d3216b142391d30
Eric Granillo <ericgranillo@users.noreply.github.com>
2022-10-18 15:55:22 +0000

    manipulating lists

---
+++ listmethods/listmeth01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# list object
+
+proto = ["ssh", "http", "https"]
+
+# printing proto list object
+
+print(proto)
+
+print(proto[1])
+
+# extending our proto list object
+
+proto.extend("dns") # this line will add d, n, s to the list
+
+print(proto)
+
+
