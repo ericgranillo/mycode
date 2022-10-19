7ec198979add888e9c8205db08998aa774004fc4
Eric Granillo <ericgranillo@users.noreply.github.com>
2022-10-19 17:15:32 +0000

    Playing with python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+print("This code will always execute.")
+
+def main():
+   print("This code belongs to the main function in module 1.")
+
+if __name__ == "__main__":
+   main()
+
+
+   # print("Module #1 Name=", __name__)
+
+#if __name__ == "__main__":
+ #  print("Code is being run directly from Python.")
+
+#else:
+#   print("Code is being run indirectly from import.")
+
+
+
