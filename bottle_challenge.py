292b402b0fa2f14d382a6ce4e67fa801ab8baf73
Eric Granillo <ericgranillo@users.noreply.github.com>
2022-10-20 20:58:17 +0000

    learning to parse log files

---
+++ bottle_challenge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+
+    # define bottles
+bottles= int(input("")
+
+for num in range(1,bottles):
+   # bottles= int(input("")
+    print(bottles - num, "Bottles of beer on the wall!\n")
+    print(bottles, "bottle of beer on the wall!",
+            bottle - num, "bottles of beer!",
+            "You take one down, pass it around! \n")
