16ec2d3b0af2e557e45739b959a5f4f56a74b070
Eric Granillo <ericgranillo@users.noreply.github.com>
2022-10-24 15:39:57 +0000

    learning about functions

---
+++ netfunct01/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+
+"""ALTA3 | EGranillo | netfunction"""
+
+import crayons
+
+def commandpush(devicecmd): #devicecmd==dict
+
+    for ip in devicecmd.keys(): # looping through the dict
+        print(f'{crayons.red("Handshaking")}. .. ... connecting with {ip}') # fstring
+        # we'll learn to write code that connects to devices here
+        for mycmds in devicecmd[ip]:
+            print(f'Attempting to sending command --> {mycmds}')
+            # we'll learn to write code that sends cmds to device here
+    return None
+
+def main():
+    """Code called at runtime"""
+    
+    # dict containing IPs mapped to a list of physical interfaces and their state
+    devicecmd = {"10.1.0.1":["interface eth1/2", "no shutdown"], "10.2.0.1":
+    ["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]}
+
+    print(f"Welcome to the {crayons.blue('network')} Device Command Pusher") # welcome message
+
+    ## get data set
+    print("\nData set found\n") # replace with function call that reads in data from file
+
+    ## run
+    commandpush(devicecmd) # call function to push commands to devices
+
+# call our main function
+
+main()
